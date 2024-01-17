c7c76c2538842a2ae02481c423690f1316d6bf49
Riliwan-dev <genius.rio121@gmail.com>
2024-01-17 21:12:48 +0100

    creation of menuItems.js file

--- React Navbar Creation/src/components/Navbar/Navbar.js
+++ React Navbar Creation/src/components/Navbar/Navbar.js
@@ -1,5 +1,19 @@
 import React, { Component } from 'react';
+import {MenuItems} from './menuItems'
 
 class Navbar extends Component{
-    render()
+    render(){
+        <nav className='NavbarItems'>
+            <h1 className='navbar-logo'>React</h1>
+
+            <div className='menu-icon'>
+
+            </div>
+        <ul>
+            {MenuItems.map((items,index) => {
+                <li className={Items.cName}></li>
+        })}
+        </ul>
+        </nav>
+    }
 }
---
+++ React Navbar Creation/src/components/Navbar/menuItems.js
@@ -0,0 +1,31 @@
+const MenuItems = [
+    {
+        title : 'Home',
+        url : '#',
+        cName : 'nav-links'
+    },
+
+    {
+        title : 'Home',
+        url : '#',
+        cName : 'nav-links'
+    },
+
+    {
+        title : 'Home',
+        url : '#',
+        cName : 'nav-links'
+    },
+
+    {
+        title : 'Home',
+        url : '#',
+        cName : 'nav-links'
+    },
+
+    {
+        title : 'Home',
+        url : '#',
+        cName : 'nav-links'
+    },
+]
