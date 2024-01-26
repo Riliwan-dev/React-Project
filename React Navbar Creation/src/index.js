eacbad2bfaddd2d4b158be37149287da3ca0746f
Riliwan-dev <genius.rio121@gmail.com>
2024-01-26 18:07:07 +0100

    modification of files

--- React Navbar Creation/src/index.js
+++ React Navbar Creation/src/index.js
@@ -3,4 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root')); 
+// ReactDOM.render(<App />, document.getElementById('root')); // error
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
