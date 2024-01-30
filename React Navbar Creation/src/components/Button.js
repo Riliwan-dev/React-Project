e67e2cb9174922915caac2f888cc125774030a8c
Riliwan-dev <genius.rio121@gmail.com>
2024-01-30 03:50:56 +0100

    Modification of some files

--- React Navbar Creation/src/components/Button.js
+++ React Navbar Creation/src/components/Button.js
@@ -4,25 +4,25 @@ import './Button.css';
 
 const STYLES = [
     'btn--primary',
-    'btn--secondary'
+    'btn--outline'
 ]
 
 const SIZES = [
-    'btn--small',
+    'btn--medium',
     'btn--large'
 ]
 
-export const Button = {(
+export const Button = ({
     children,
-    types,
-    Onclick,
+    type,
+    onClick,
     buttonStyle,
     buttonSize
-)} => {
+}) => {
 
     const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0]
 
-    const checkButtonSize = SIZES.includes(buttonSize ? buttonStyle : SIZES[0]
+    const checkButtonSize = SIZES.includes(buttonSize) ? buttonStyle : SIZES[0]
 
     return(
 
