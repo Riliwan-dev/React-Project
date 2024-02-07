eb322021a27d2c972ca7f06784ae1ca4b21317c9
Riliwan-dev <genius.rio121@gmail.com>
2024-02-07 10:46:29 +0100

    modification of files

--- React Shopping Cart Ecommerce website/src/assest/product.js
+++ React Shopping Cart Ecommerce website/src/assest/product.js
@@ -1,7 +1,8 @@
 export const Product = [
     {
         id: 1,
-        name: "Iphone X",
-        description: "The iPhone X is the latest model of Iphones. It has a
-    }
+        productName: "Iphone X",
+        price: 999.0,
+        productImage: 
+    },
 ]
