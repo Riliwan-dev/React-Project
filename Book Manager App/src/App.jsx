c3d2db15c513d35340df6861556498efe6b1dd4d
Riliwan-dev <genius.rio121@gmail.com>
2025-11-16 17:37:55 +0100

    Creating of Components and Pages Folders and adding of files

--- Book Manager App/src/App.jsx
+++ Book Manager App/src/App.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
+import { Toaster } from 'react-hot-toast'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Shop from './pages/Shop'
+import CategoryShop from './pages/CategoryShop'
+import ProductDetails from './pages/ProductDetails'
+import Blog from './pages/Blog'
+import Contact from './pages/Contact'
+import Footer from './components/Footer'
+import Header from './components/Header'
 
 const App = () => {
   return (
     <main>
+      <Header />
+      <Toaster position='bottom-right' />
       <Routes>
-        <link rel="stylesheet" href="" />
-    <span className='bg-red-500'>App</span>
+        <Route path='/' element={<Home />} />
+        <Route path='/shop' element={<Shop />} />
+        <Route path='/shop/:category' element={<CategoryShop />} />
+        <Route path='/shop/:category/:id' element={<ProductDetails />} />
+        <Route path='/blog' element={<Blog />} />
+        <Route path='/contact' element={<Contact />} />
       </Routes>
+      <Footer />
     </main>
   )
 }
