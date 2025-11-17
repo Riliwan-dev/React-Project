ce069c9129fef6c8ca778e9e495d338c68ba2c9e
Riliwan-dev <genius.rio121@gmail.com>
2025-11-17 17:56:26 +0100

    Modification of Header.jsx files

--- Book Manager App/src/components/Header.jsx
+++ Book Manager App/src/components/Header.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import logoImg from "../assets/logo.png";
+import userImg from "../assets/user.png";
+import { FaBars, FaBarsStaggered } from "react-icons/fa6";
+import { FaSearch } from "react-icons/fa";
+import { RiUserLine } from "react-icons/ri";
+import Navbar from './Navbar';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+        {/* LOGO */}
+        <div className="flex flex-1">
+            <Link to={"/"} className='bold-22 xl:bold-28 flex items-end gap-1'>
+            <img src={logoImg} alt="" className="hidden sm:block"/>
+            <div className="sm:relative top-1 5">Zibook<span className="text-secondary">a.</span></div>
+            </Link>
+        </div>
+        {/* NAVBAR FOR MOBILE & DESKTOP */}
+        <div className="flex-1">
+            <Navbar />
+        </div>
+    </header>
   )
 }
 
